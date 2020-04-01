025ad2439b0e171e12cab773069668b8b014c321
Kyle <kylehinckley@hotmail.com>
2020-04-01 10:04:56 -0600

    Baba Yaga - Changes

---
+++ BabaYagaUnity/Assets/Scripts/Collect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    public GameObject particleEffect;
+    public Renderer myRenderer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        Instantiate(particleEffect, transform);
+        myRenderer = gameObject.GetComponent<Renderer>();
+        myRenderer.enabled = false;
+    }
+}
