1262eeb3a3529a620eb69554fa1bae7ec0e9ea65
Kyle <kylehinckley@hotmail.com>
2020-04-07 20:50:09 -0600

    Baba Yaga - Final Lighting/Texture Pass

--- BabaYagaUnity/Assets/Scripts/Collect.cs
+++ BabaYagaUnity/Assets/Scripts/Collect.cs
@@ -3,12 +3,12 @@
 public class Collect : MonoBehaviour
 {
     public GameObject particleEffect;
-    public Renderer myRenderer;
+    public Transform intendedParent;
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
-        Instantiate(particleEffect, transform);
-        myRenderer = gameObject.GetComponent<Renderer>();
-        myRenderer.enabled = false;
+        GameObject instance = Instantiate(particleEffect, transform.position, transform.rotation, transform.parent);
+        // instance.transform.parent = gameObject.transform.parent;
+        gameObject.SetActive(false);
     }
 }
---
+++ BabaYagaUnity/Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+    public GameObject playerObject;
+    private Vector3 followVector;
+    private CharacterController playerController;
+    private Transform transformPosition;
+    private bool inDamageZone;
+
+    void Start()
+    {
+        playerController = playerObject.GetComponent<CharacterController>();
+        transformPosition = gameObject.transform;
+    }
+    void Update()
+    {
+        var followPosition = playerObject.transform.position;
+        
+        if (playerController.isGrounded)
+        {
+            
+            followVector.x = followPosition.x;
+            followVector.y = followPosition.y - 5;
+            followVector.z = followPosition.z;
+        }
+        else
+        {
+            followVector.x = followPosition.x;
+            followVector.z = followPosition.z;
+        }
+
+        transformPosition.position = followVector;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inDamageZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inDamageZone = false;
+        }
+    }
+}
