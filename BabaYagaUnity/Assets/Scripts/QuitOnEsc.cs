195e58e92a8a1fd5abdf72bb539a36bcda045c99
Kyle <kylehinckley@hotmail.com>
2020-04-22 12:28:50 -0600

    Baba Yaga - Little Mat Work

---
+++ BabaYagaUnity/Assets/Scripts/QuitOnEsc.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class QuitOnEsc : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
